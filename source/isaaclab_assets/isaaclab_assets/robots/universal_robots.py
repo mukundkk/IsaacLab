3e2666fba8a439a8cf76c7fd656c0e1bdf50686f
Mukund Krishnakumar <38397833+mukundkk@users.noreply.github.com>
2025-09-17 12:41:57 -0700

    Enable contact sensors in universal robots config
    Signed-off-by: Mukund Krishnakumar <38397833+mukundkk@users.noreply.github.com>

--- source/isaaclab_assets/isaaclab_assets/robots/universal_robots.py
+++ source/isaaclab_assets/isaaclab_assets/robots/universal_robots.py
@@ -30,7 +30,7 @@ UR10_CFG = ArticulationCfg(
             disable_gravity=False,
             max_depenetration_velocity=5.0,
         ),
-        activate_contact_sensors=False,
+        activate_contact_sensors=True,
     ),
     init_state=ArticulationCfg.InitialStateCfg(
         joint_pos={
